8413988e8b2a17afce9c9a8b7651690e3417f37f
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-06-13 13:52:47 +0200

    Add usestate & context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [context, setContext] = useState({});
   return (
     <div className="app">
-      <Outlet />
+      <Outlet context={[context, setContext]} />
     </div>
-  );
+  )
 }
 
 export default App;
