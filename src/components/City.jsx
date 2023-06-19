700583b1d57bb307f33ebe404224ac8e0f816191
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-06-19 12:23:58 +0200

    Added city component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import useAxios from "../useAxios";
+import { TbMapPin } from "react-icons/Tb";
+
+const StyledDiv = styled.div`
+  height: 30px;
+  width: 180px;
+`;
+
+const StyledTbMapPin = styled(TbMapPin)`
+  color: #5533ea;
+`;
+
 const City = () => {
-  return <p>New York City</p>;
+  const [data, error, loading] = useAxios("id");
+  return (
+    <>
+      {error && <p>Der opstod en fejl...</p>}
+      {loading && <p>loading...</p>}
+      {data && (
+        <>
+          {data.animals.map((animal) => (
+            <div className="Flex Center flex-start">
+              <StyledTbMapPin />
+              <p>{animal.contact.address.city}</p>
+            </div>
+          ))}
+        </>
+      )}
+    </>
+  );
 };
 
 export default City;
